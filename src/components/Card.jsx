f2b32e4bec43b37b0560b01f0b575b71c0c6a8d5
Callum Lees <hi@c-l.dev>
2022-04-23 17:50:43 +0100

    Created Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import qrcode from '../images/image-qr-code.png';
 
 const Card = () => {
   
   return (
 
-    <div className=''>
+    <>
 
-    </div>
+      <div className='flex h-screen mx-auto justify-center items-center'>
+
+        <div className='w-80 bg-white text-center p-4 rounded-2xl'>
+  
+          <img 
+            src={qrcode} 
+            alt='QR code image' 
+            className='rounded-2xl' 
+          />  
+      
+          <div className='flex flex-col px-2'>
+
+            <h2 className='text-2xl text-dark-blue mt-6 font-outfit font-semibold'>Improve your front-end skills by building projects</h2>
+
+            <p className='mb-4 font-outfit text-gray-blue mt-4 font-thin'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </>
 
   ); 
 
