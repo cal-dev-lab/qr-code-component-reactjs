74cb35f458165464ccfcd614c631fbed7c55cd2e
Callum Lees <hi@c-l.dev>
2022-04-23 17:52:31 +0100

    Added white colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   ],
   theme: {
     colors: {
+      'white': 'hsl(0, 0%, 100%)',
       'light-gray': 'hsl(212, 45%, 89%)',
       'gray-blue': 'hsl(220, 15%, 55%)',
       'dark-blue': 'hsl(218, 44%, 22%)',
